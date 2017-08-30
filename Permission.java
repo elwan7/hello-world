15d983a4af74cc2cf7268f4ba13bd862ce2d87e7
Ahmed Muhammed Galal Elwan <ahmed.elwan568@gmail.com>
2017-08-31 01:28:18 +0200

    Update Permission.java

--- Permission.java
+++ Permission.java
@@ -4,6 +4,7 @@
 /* Permissions in manifest file ----- 
 <uses-permission android:name="android.permission.ACCESS_COARSE_LOCATION"></uses-permission>
     <uses-permission android:name="android.permission.ACCESS_FINE_LOCATION"></uses-permission>
+    <uses-permission android:name="android.permission.READ_CONTACTS"></uses-permission>
     */
 
 
@@ -11,7 +12,7 @@
     void checkUserPermissions(){
         //check Version
         if ( Build.VERSION.SDK_INT >= 23){
-            //check if  my app have this permission granted previously ?? or first time ?!
+            //check if  my app have this permission granted previously ?? or this first time ?!
             if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) !=
                     PackageManager.PERMISSION_GRANTED ){
                 //Display Screen to request perm from the user
@@ -53,6 +54,27 @@
         txtLocation.setText("Longitude = "+ String.valueOf(location.getLongitude()) +
                 "Altitude = "+String.valueOf(location.getLatitude()));
     }
+
+
+    void readContacts(){
+        //read name, telephone
+        //extract data from another App via ContentProvider
+        Cursor cursor = getContentResolver().query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI ,
+                null, null, null, null);
+
+        //loop to retrieve data  with row by row
+        if (cursor.moveToFirst() /*This method will return false if the cursor is empty*/){
+            do{
+                //bring data from DB in that arrayList ..
+                String name = cursor.getString(cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME));
+                String tel = cursor.getString(cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER));
+                contactList.add(new ContactItems(name, tel));
+
+            }while (cursor.moveToNext());
+            /** Move the cursor to the next row.
+             This method will return false if the cursor is already past the last entry in the result set.*/
+        }
+    }//end of readContacts Method
 
 
 
