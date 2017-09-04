dfb96f382fbb0e0a6e4dd79d3cea988815951a24
Ahmed Muhammed Galal Elwan <ahmed.elwan568@gmail.com>
2017-09-05 01:14:03 +0200

    Update Notifications.java

--- Notifications.java
+++ Notifications.java
@@ -1,14 +1,18 @@
- 
- int notId = 0;
- //1st way: little complex
 
+        //1st way: little complex
+
+        int notId = 0;
         //build my Notification
         NotificationCompat.Builder nBuild = new NotificationCompat.Builder(this);
         //set properties
         nBuild.setContentTitle("dangerous")
                 .setContentText("it will run soon!")
                 .setSmallIcon(R.drawable.caution2);
-        //display my Notify
+        //display my Notification
         NotificationManager manager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
         manager.notify(notId, nBuild.build());
         notId++;
+
+        //2nd way: simple
+
+        
